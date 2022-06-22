34113b65f889e0ffed719db894c36ef89e6d024c
onyelo.king <kingsley@intelligentinnovations.co>
2022-06-22 19:08:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './components/home/Home'
+import About from './components/about/About'
+import CourseHome from './components/allcourses/CourseHome'
+import Team from './components/team/Team'
+import Price from './components/pricing/Price'
+import Blog from './components/blog/Blog'
+import Contact from './components/contact/Contact'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/courses' element={<CourseHome />} />
+          <Route path='/team' element={<Team />} />
+          <Route path='/price' element={<Price />} />
+          <Route path='/journal' element={<Blog />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
